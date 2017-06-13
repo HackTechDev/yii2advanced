79e671d8d101460650c1b99d3f6966ba5bdcfa25
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2017-06-13 20:51:37 +0200

    Version 0.0.1

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'hfhzLkz2gowZbP4w6TS55sN6RntQMuH9',
+        ],
+    ],
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'vKrSnqNxg-pbtDJo6UK7xj09K4iF1sYS',
+        ],
+    ],
+];
